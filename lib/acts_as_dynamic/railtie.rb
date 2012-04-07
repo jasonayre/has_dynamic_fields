9900b75cdd1fae10089f3e937b2133563bd3ba6f
Jason Ayre <jasonayre@gmail.com>
2012-04-07 10:38:09 -0600

    version 0.1

--- lib/acts_as_dynamic/railtie.rb
+++ lib/acts_as_dynamic/railtie.rb
@@ -4,7 +4,11 @@ require "rails"
 module ActsAsDynamic
   class Railtie < Rails::Railtie
     initializer 'acts_as_dynamic.ar_extensions' do |app|
+      require 'acts_as_dynamic/has_dynamic_field_values' if defined?(Rails)
+
       ActiveRecord::Base.extend ActsAsDynamic::Base
+      ActiveRecord::Base.send :extend, ActsAsDynamic::HasDynamicFieldValues
+ 
     end
   end
 end
